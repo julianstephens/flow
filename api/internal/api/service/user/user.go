d159ba2d600e59585e676f4e159e534d1d10a38c
Julian Stephens <julian@julianstephens.net>
2022-08-15 13:31:42 -0400

    Add create user endpoint

--- api/internal/api/service/user/user.go
+++ api/internal/api/service/user/user.go
@@ -2,21 +2,32 @@ package user
 
 import (
 	ctrl "api/internal/api/controller"
+	"api/internal/models"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
-// func CreateUser(c *gin.Context) {
-// }
-
 var user ctrl.User
+
+func CreateUser(c *gin.Context) {
+	var req models.CreateUserInput
+	c.BindJSON(&req)
+
+	res, err := user.CreateUser(req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"data": &res, "message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": &res, "message": "Created."})
+}
 
 func GetUser(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	res, err := ctrl.User.GetUser(user, id)
+	res, err := user.GetUser(id)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"data": &res, "message": err.Error()})
 		return
