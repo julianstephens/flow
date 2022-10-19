0edd7f5d0150cd2652f7baf1450ee0e4262adbf4
Julian Stephens <julian@julianstephens.net>
2022-10-19 12:36:45 -0400

    Clear env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,13 +1,14 @@
 
-import {IEnvironment} from "@app/common/common.models";
+  import {IEnvironment} from "@app/common/common.models";
 
-export const environment: IEnvironment = {
-  production: false,
-  auth: {
-    domain: "dev-8nx0w2kt.us.auth0.com",
-    clientId: "U1uSdz2IkMborC9NM9sihV8DD21DQSV9",
-    audience: "http://localhost:3000",
-    redirectUri: "http://localhost:4200/auth/redirect",
-  },
-  apiUri: "http://localhost:3000/api",
-};
+  export const environment: IEnvironment = {
+    production: false,
+    auth: {
+      domain: "",
+      clientId: "",
+      audience: "",
+      redirectUri: "",
+    },
+    apiUri: "",
+  };
+  
